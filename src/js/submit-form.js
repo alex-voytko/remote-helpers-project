9a86be3834f7311c24ea7b36d3db197bb1bc3e73
alex-voytko <voytko1994@gmail.com>
2022-05-11 22:47:18 +0300

    3.5.0

--- src/js/submit-form.js
+++ src/js/submit-form.js
@@ -8,9 +8,12 @@ export default formRef.addEventListener('submit', formZipSubmit);
 function formZipSubmit(e) {
   e.preventDefault();
   const formData = new FormData(e.currentTarget);
-  addUserData(formData).then(data => {
-    console.log(data);
-    setTimeout(openThankYouModal, 2000);
-  });
+  addUserData(formData)
+    .then(data => {
+      console.log(data);
+      setTimeout(openThankYouModal, 2000);
+    })
+    .catch(error => console.log(error.message));
+
   formRef.reset();
 }

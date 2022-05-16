de0fd56826d455260ce53d75091dc47cf04f3349
alex-voytko <voytko1994@gmail.com>
2022-05-16 16:17:54 +0300

    3.6.6.

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,56 @@
-import fields from './fields.js';
+const fields = [
+  {
+    type: 'text',
+    name: 'client_name',
+    placeholder: 'Your name',
+    required: true,
+    label: 'Name*',
+  },
+  {
+    type: 'email',
+    name: 'client_email',
+    placeholder: 'Email',
+    required: true,
+    label: 'E-mail*',
+  },
+  {
+    type: 'tel',
+    name: 'client_phone',
+    placeholder: '+xxxxxxxxxxxx',
+    required: true,
+    pattern: '',
+    label: 'Phone number*',
+  },
+  {
+    type: 'text',
+    name: 'note',
+    required: true,
+    placeholder: '2 p.m. - 5 p.m.',
+    label: 'Better time for call*',
+  },
+  {
+    type: 'text',
+    name: 'global_company_name',
+    placeholder: 'Your company',
+    required: false,
+    hidden: true,
+    value: 'Worksuite Demo Company',
+  },
+  {
+    type: 'text',
+    name: 'project_company',
+    placeholder: 'Your company',
+    required: false,
+    hidden: true,
+    value: 'RHS',
+  },
+];
+const refs = {
+  backdrop: document.querySelector('.backdrop'),
+  modalBtn: document.querySelector('.modal-btn'),
+  form: document.querySelector('.form'),
+};
+const url = 'https://crm.rhelpers.com/api/v1/leads-public';
 
 window.addEventListener(
   'DOMContentLoaded',
@@ -9,13 +61,11 @@ function drowForm(fields, selector) {
   const formRef = document.querySelector(selector);
   formRef.insertAdjacentHTML('beforeend', buildInputs(fields));
 }
-
-function buildInputs(fields) {
+function buildInputs(formFields) {
   let str = '';
   let openLabel = '';
   let closeLabel = '';
-
-  fields.forEach(field => {
+  formFields.forEach(field => {
     if (field.label) {
       openLabel = `<label>${field.label}`;
       closeLabel = `</label>`;
@@ -33,7 +83,6 @@ function buildInputs(fields) {
           field.required ? 'required' : ''
         }></textarea>${closeLabel}`;
         break;
-
       case 'select':
         if (!field.options || !field.options.length) {
           break;
@@ -44,7 +93,6 @@ function buildInputs(fields) {
           .map(option => `<option>${option.value}</option>`)
           .join('')}</select>`;
         break;
-
       default:
         str += `${openLabel}<input type="${field.type}" ${
           field.class ? `class="${field.class}"` : ''
@@ -58,4 +106,38 @@ function buildInputs(fields) {
     }
   });
   return str;
+}
+
+refs.form.addEventListener('submit', formZipSubmit);
+
+function formZipSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  addUserData(formData)
+    .then(data => {
+      console.log(data);
+      setTimeout(openThankYouModal, 1000);
+    })
+    .catch(error => console.log(error.message));
+  refs.form.reset();
+}
+async function addUserData(userData) {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: userData,
+  });
+
+  return response.json();
+}
+
+function openThankYouModal() {
+  refs.backdrop.classList.add('is-visible');
+  refs.modalBtn.addEventListener('click', closeThankYouModal);
+  refs.backdrop.addEventListener('click', onBackDropClick);
+}
+function closeThankYouModal() {
+  refs.backdrop.classList.remove('is-visible');
+}
+function onBackDropClick(e) {
+  if (e.target === e.currentTarget) closeThankYouModal();
 }
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,3 +1,121 @@
+const desktopLogosRef = document.querySelector('#js-swiper');
+const desktopViewport = 1200;
+let desktopMode = false;
+
+document.addEventListener('DOMContentLoaded', onLoadViewportCheck);
+window.addEventListener('resize', _.throttle(onViewportChange, 500), false);
+
+const logos = `<div class="logo-wrapper">
+        <div class="logo-wrapper__first-string">
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 290px; display: flex; align-items: center"
+          >
+            <img
+              src="./img/clients/finance4learning.png"
+              alt="Finance4Learning"
+            />
+          </div>
+          <div
+            class="company-logo-view"
+            style="width: 220px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/fancyames.png" alt="fancyames" />
+          </div>
+        </div>
+        <div class="logo-wrapper__second-string">
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 157px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/chiko.png" alt="chiko" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 144px; display: flex; align-items: center"
+          >
+            <img
+              src="./img/clients/q-online-d-m-agancy.png"
+              alt="Q Online Digital Marketing Agency"
+            />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 186px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/onward-click.png" alt="OnwardClick" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 204px; display: flex; align-items: center"
+          >
+            <img
+              src="./img/clients/silicon-publishing.png"
+              alt="Silicon Publishing"
+            />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 220px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/cygobel.png" alt="cygobel" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 202px; display: flex; align-items: center"
+          >
+            <img
+              src="./img/clients/change-consult-scotland.png"
+              alt="Change Consulting Scotland"
+            />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 191px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/taketask.png" alt="TakeTask" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 225px; display: flex; align-items: center"
+          >
+            <img
+              src="./img/clients/metropolitan-movers.png"
+              alt="Metropolitan Movers"
+            />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 220px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/evolution-u.jpg" alt="Evolution U" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 220px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/edgency.jpg" alt="EDGency" />
+          </div>
+          <div
+            class="swiper-slide company-logo-view"
+            style="width: 220px; display: flex; align-items: center"
+          >
+            <img src="./img/clients/collect-media.png" alt="Collect Media" />
+          </div>
+          <div
+            class="company-logo-view"
+            style="
+              width: 220px;
+              display: flex;
+              align-items: center;
+              margin-right: 0;
+            "
+          >
+            <img src="./img/clients/convertise.png" alt="Convertise" />
+          </div>
+        </div>
+      </div>`;
+
 new Swiper('.swiper', {
   navigation: {
     nextEl: '.svg-container-2',
@@ -7,5 +125,29 @@ new Swiper('.swiper', {
   slidesPerView: 'auto',
   spaceBetween: 20,
   centeredSlides: true,
-  initialSlide: 3,
+  initialSlide: 7,
 });
+
+function onLoadViewportCheck(e) {
+  if (e.target.body.clientWidth >= desktopViewport) {
+    desktopMode = true;
+    desktopLogosRef.insertAdjacentHTML('beforeend', logos);
+    return;
+  }
+  desktopLogosRef.innerHTML = '';
+}
+
+function onViewportChange(e) {
+  if (!desktopMode) {
+    if (e.target.innerWidth >= desktopViewport) {
+      desktopMode = true;
+      desktopLogosRef.insertAdjacentHTML('beforeend', logos);
+    }
+  }
+  if (desktopMode) {
+    if (e.target.innerWidth < desktopViewport) {
+      desktopMode = false;
+      desktopLogosRef.innerHTML = '';
+    }
+  }
+}
